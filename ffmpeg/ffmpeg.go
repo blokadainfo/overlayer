43224befee6e19ad089e16e337076b3524da712e
Đorđe Mančić <djordje@mancic.org>
2025-10-28 03:20:10 +0100

    feat: logo scaling based on resolution

--- ffmpeg/ffmpeg.go
+++ ffmpeg/ffmpeg.go
@@ -10,10 +10,12 @@ import (
 )
 
 func CreateFFmpegCommand(oc config.OverlayConfig, sc config.StreamConfig) *exec.Cmd {
+	referenceHeight := 1080 // Video height at which the logo will remain unscaled (exactly width x height pixels as defined in the config)
+
 	cmdArgs := []string{
 		"-i", sc.Src,
 		"-i", oc.LogoPath,
-		"-filter_complex", fmt.Sprintf("[1:v]scale=%d:%d,format=rgba,colorchannelmixer=aa=0.75[logo];[0:v][logo]overlay=24:(main_h-overlay_h)/2", oc.LogoWidth, oc.LogoHeight),
+		"-filter_complex", fmt.Sprintf("[1:v]format=rgba,colorchannelmixer=aa=0.75[logo];[logo][0:v]scale=w=%d*(rh/%d):h=%d*(rh/%d)[logo_scaled];[0:v][logo_scaled]overlay=24*(main_h/%d):(main_h-overlay_h)/2", oc.LogoWidth, referenceHeight, oc.LogoHeight, referenceHeight, referenceHeight),
 		"-c:v", "libx264",
 		"-preset", "ultrafast",
 		"-c:a", "aac",
